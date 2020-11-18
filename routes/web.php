cd0e0103fc1fd5d73f7f8430cbbf27361c7f6ccb
Faisal611 <71620505+Faisal611@users.noreply.github.com>
2020-11-18 15:13:34 +0600

    firstLaravel

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return 'hello Laraval';
+});
+
+Route::get('/bitm', function(){
+    return 'Hello Bitm';
+
+});
+
+Route::get('/', function (){
+    return view('home');
+
+});
+
+//Route ::get('/', 'StudentController@index');
+
+Route ::get('/',[
+    'uses' => 'StudentController@index',
+    'as' => '/'
+]);
+
+Route::get('/about',[
+    'uses' => 'StudentController@about',
+    'as' => '/about'
+
+]);
